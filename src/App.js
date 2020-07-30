360f7c9beaa887538d6a610816c3c0bcd94e4ad2
lospericos <67882228+lospericos@users.noreply.github.com>
2020-07-29 17:13:18 -0700

    update src with air files

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ import './App.css';
 
 class App extends Component {
 
+  /*
+
+  add ability to update model here 
+
+  */
   
   render() {
     return (
       <div className="App">
         <Container>
           <Navbar collapseOnSelect className="app-nav-bar" variant="dark" expand="lg">
-            <Navbar.Brand href="/">PokeDetective</Navbar.Brand>
+            <Navbar.Brand href="/">Pok&eacute; &#x1F50D; Detective</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="">
                 <Link className="nav-link" to="/">Detect</Link>
+                <Link className="nav-link" to="/list">Pok&eacute;List</Link>
                 <Link className="nav-link" to="/about">About</Link>
               </Nav>
             </Navbar.Collapse>
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import About from './pages/About';
+import List from './pages/List';
 import Detect from './pages/Detect';
 import NotFound from './pages/NotFound';
 
 export default ({ childProps }) =>
   <Switch>
-    {/* <Route path="/" exact component={Detect} props={childProps} /> */}
+    <Route path="/" exact component={Detect} props={childProps} />
+    {/* <Route path="/list" exact component={List} props={childProps} /> */}
     <Route path="/about" exact component={About} props={childProps} />
     <Route component={NotFound} />
   </Switch>;
