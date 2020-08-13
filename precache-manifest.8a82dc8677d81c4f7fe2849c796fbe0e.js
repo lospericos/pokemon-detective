64cc148013b1ab2a2592635ab7a2add4f82a518b
Greg Han <gerg@gregs-mbp.lan>
2020-08-13 01:33:53 -0700

    Updates

---
+++ precache-manifest.8a82dc8677d81c4f7fe2849c796fbe0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9be7c38c7709fe76bb31fa162ee87e5",
+    "url": "/pokemon-detective/index.html"
+  },
+  {
+    "revision": "ff0ec7cc676cf2b38314",
+    "url": "/pokemon-detective/static/css/2.ec2531fa.chunk.css"
+  },
+  {
+    "revision": "7e5a819bb9940a58cc48",
+    "url": "/pokemon-detective/static/css/main.35a299ea.chunk.css"
+  },
+  {
+    "revision": "ff0ec7cc676cf2b38314",
+    "url": "/pokemon-detective/static/js/2.efb19933.chunk.js"
+  },
+  {
+    "revision": "41de880978cbf55dd100255f8fd58ddc",
+    "url": "/pokemon-detective/static/js/2.efb19933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e5a819bb9940a58cc48",
+    "url": "/pokemon-detective/static/js/main.01cac4ce.chunk.js"
+  },
+  {
+    "revision": "9b21f60d97799c3b6713",
+    "url": "/pokemon-detective/static/js/runtime-main.8e99f17f.js"
+  }
+]);
